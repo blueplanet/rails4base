f8b841923f67ba6b629f934e135e7478f5df963a
blueplanet <erguolinge@gmail.com>
2016-04-26 06:32:30 +0900

    bootstrap form

--- app/controllers/bootstrap_forms_controller.rb
+++ app/controllers/bootstrap_forms_controller.rb
@@ -2,10 +2,12 @@ class BootstrapFormsController < ApplicationController
   skip_before_action :authenticate_user!
 
   def new
+    @post = Post.new
   end
 
   def create
-    if Post.new(params.require(:post).permit(:title, :body, :draft, :published_at)).save
+    @post = Post.new(params.require(:post).permit(:title, :body, :draft, :published_at))
+    if @post.save
       redirect_to root_path
     else
       render :new
